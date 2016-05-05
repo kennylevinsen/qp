2979647b53602036d7af2fbbc66d11dc8dd8620b
Kenny Levinsen <w@kl.wtf>
2016-05-05 14:41:32 +0200

    Revert "Remove locking from Encoder.WriteMessage"
    This reverts commit f1eeddc1c759b49d80c4a04b750c09243031471a.

--- container.go
+++ container.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+	"sync"
 )
 
 var (
@@ -38,8 +39,8 @@ type Message interface {
 	GetTag() Tag
 }
 
-// Encoder handles writes encoded messages to an io.Writer. An Encoder is not
-// thread safe. Only one goroutine may call WriteMessage at a time.
+// Encoder handles writes encoded messages to an io.Writer. Encoder is thread
+// safe, and may be called in parallel from arbitrary goroutines.
 type Encoder struct {
 	// Protocol is the protocol codec used for encoding messages.
 	Protocol Protocol
@@ -50,6 +51,10 @@ type Encoder struct {
 	// MessageSize is the maximum message size negotiated for the protocol. It
 	// is used to enforce a limit on writes.
 	MessageSize uint32
+
+	// writeLock is used to synchronize writes. Without it, messages would end
+	// up interleaved and incomprehensible.
+	writeLock sync.Mutex
 }
 
 // WriteMessage encodes a message and writes it to the Encoders associated
@@ -76,6 +81,9 @@ func (e *Encoder) WriteMessage(m Message) error {
 	header = make([]byte, HeaderSize)
 	binary.LittleEndian.PutUint32(header[0:4], uint32(len(msgbuf)+HeaderSize))
 	header[4] = byte(mt)
+
+	e.writeLock.Lock()
+	defer e.writeLock.Unlock()
 
 	if _, err = e.Writer.Write(header); err != nil {
 		return err
